988f174f3253786e9c4484480f119ff1b418663f
HUHMUMU <hurjun07@naver.com>
2023-04-24 10:06:47 +0900

    우상이 제외 병합 완료

--- src/main/java/com/acorn/chapspring/mapper/WaitingMapper.java
+++ src/main/java/com/acorn/chapspring/mapper/WaitingMapper.java
@@ -1,18 +1,18 @@
-package com.acorn.chapspring.mapper;
-import com.acorn.chapspring.dto.*;
-import org.apache.ibatis.annotations.Mapper;
-import java.util.List;
-
-@Mapper
-public interface WaitingMapper {
-
-    List<WaitingDto> getWaitingList();
-    WaitingDto getWaiting(int id);
-    void insertWaiting(WaitingDto waitingDto);
-    void updateWaiting(WaitingDto waitingDto);
-    void deleteWaiting(int id);
-
-}
-
-
-// 보완 수정해야함
+//package com.acorn.chapspring.mapper;
+//import com.acorn.chapspring.dto.*;
+//import org.apache.ibatis.annotations.Mapper;
+//import java.util.List;
+//
+//@Mapper
+//public interface WaitingMapper {
+//
+//    List<WaitingDto> getWaitingList();
+//    WaitingDto getWaiting(int id);
+//    void insertWaiting(WaitingDto waitingDto);
+//    void updateWaiting(WaitingDto waitingDto);
+//    void deleteWaiting(int id);
+//
+//}
+//
+//
+//// 보완 수정해야함
--- src/main/java/com/acorn/chapspring/service/WaitingService.java
+++ src/main/java/com/acorn/chapspring/service/WaitingService.java
@@ -1,24 +1,24 @@
-package com.acorn.chapspring.service;
-
-import com.acorn.chapspring.dto.WaitingDto;
-import com.acorn.chapspring.mapper.WaitingMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-@Service
-public class WaitingService {
-    @Autowired
-    private WaitingMapper waitingMapper;
-
-    boolean addWaiting(WaitingDto waitingDto) {
-        return false;
-    }
-
-    public void insertUser(WaitingDto WaitingDto) {
-        waitingMapper.insertUser(WaitingDto);
-    }
-    boolean deleteWaiting(int waitingId);
-}
-
-// 보완수정해야함
+//package com.acorn.chapspring.service;
+//
+//import com.acorn.chapspring.dto.WaitingDto;
+//import com.acorn.chapspring.mapper.WaitingMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//@Service
+//public class WaitingService {
+//    @Autowired
+//    private WaitingMapper waitingMapper;
+//
+//    boolean addWaiting(WaitingDto waitingDto) {
+//        return false;
+//    }
+//
+//    public void insertUser(WaitingDto WaitingDto) {
+//        waitingMapper.insertUser(WaitingDto);
+//    }
+//    boolean deleteWaiting(int waitingId);
+//}
+//
+//// 보완수정해야함
