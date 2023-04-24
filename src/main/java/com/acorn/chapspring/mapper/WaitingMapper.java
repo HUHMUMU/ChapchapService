d281903878625c24f4831f98081c349baa6b8c45
Tim <developerTim@naver.com>
2023-04-24 17:07:31 +0900

    윤식 커밋 33

--- src/main/java/com/acorn/chapspring/mapper/WaitingMapper.java
+++ src/main/java/com/acorn/chapspring/mapper/WaitingMapper.java
@@ -8,11 +8,8 @@ public interface WaitingMapper {
 
     List<WaitingDto> getWaitingList();
     WaitingDto getWaiting(int id);
-    void insertWaiting(WaitingDto waitingDto);
-    void updateWaiting(WaitingDto waitingDto);
-    void deleteWaiting(int id);
+    int addWaiting(WaitingDto waitingDto);
+    int modifyWaiting(WaitingDto waitingDto);
+    int deleteWaiting(int id);
 
 }
-
-
-// 보완 수정해야함
---
+++ src/main/java/com/acorn/chapspring/service/WaitingService1.java
@@ -0,0 +1,8 @@
+//package com.acorn.chapspring.service;
+//@Service
+//public interface WaitingService1 {
+//
+//}
+
+
+// 잘못만들어서 수정
