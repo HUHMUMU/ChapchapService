172cd098c613ca63889bada214f00caba38ee1b5
jaeyeonjjang <wodus0936@gmail.com>
2023-04-26 12:33:19 +0900

    검색 구현

--- src/main/java/com/acorn/chapspring/dto/ChapstoryPageDto.java
+++ src/main/java/com/acorn/chapspring/dto/ChapstoryPageDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class ChapstoryPageDto {
     enum BoardsType{
-        chap_num, user_id, title, content, viewcount, post_time, likes
+        chap_num, user_id, title, content, viewcount, post_time, likes, nickname
     }
 
     enum DirectType{
