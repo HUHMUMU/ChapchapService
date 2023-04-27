5d32d5b0edb7fff880b29968a8a8e0a06ebd599b
seung ho <316s5615@naver.com>
2023-04-27 17:27:09 +0900

    노트북으로 작업하던거 컴으로 하던거랑 합쳐야함

--- src/main/java/com/acorn/chapspring/controller/MainController.java
+++ src/main/java/com/acorn/chapspring/controller/MainController.java
@@ -1,21 +1,36 @@
 package com.acorn.chapspring.controller;
 
-import com.acorn.chapspring.dto.ChapDealDto;
 import com.acorn.chapspring.dto.StoresDto;
-import com.acorn.chapspring.service.StoreService;
+import com.acorn.chapspring.service.MainService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @AllArgsConstructor //모든 필드를 pojo 형식의 생성자로 자동 생성
 @Controller //< @Component 요청과 응답을 처리 가능
-@RequestMapping("/mainPage")
+@RequestMapping("/")
 @Log4j2 //log 필드로 로그남길 수 있다.(파일로 저장 가능[유지기간,성질])
 public class MainController {
-    private StoreService storeService;
+    private MainService mainService;
+
+    @GetMapping("/searchStore.do")
+    public String searchStoreFrom(){
+        return "/"; //렌더할 뷰
+    }
+    @PostMapping("/searchStore.do")
+    public String searchBestStore(
+            @RequestParam("siAddress") String siAdd,
+            @RequestParam("guAddress") String guAdd,
+            Model model){
+        StoresDto store = mainService.getStoreByAddressReviews(siAdd, guAdd);
+        model.addAttribute("store",store);
+        return "redirect:/";
+    }
 }
--- src/main/java/com/acorn/chapspring/service/MainService.java
+++ src/main/java/com/acorn/chapspring/service/MainService.java
@@ -7,7 +7,5 @@ import java.util.List;
 
 @Service
 public interface MainService {
-    StoreServiceImp findByReviewsCount(String storeName);
-
-
+    StoresDto getStoreByAddressReviews(String siAdd,String guAdd);
 }
