dea227baa4c04b1af0649888923039c6e9e9c04a
HUHMUMU <hurjun07@naver.com>
2023-04-27 09:19:59 +0900

    윤식,재연,희선,원준 병합완료

--- src/main/java/com/acorn/chapspring/mapper/WaitingMapper.java
+++ src/main/java/com/acorn/chapspring/mapper/WaitingMapper.java
@@ -1,15 +1,16 @@
 package com.acorn.chapspring.mapper;
 import com.acorn.chapspring.dto.*;
 import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
 
 @Mapper
 public interface WaitingMapper {
 
-    List<WaitingDto> getWaitingList();
-    WaitingDto getWaiting(int id);
-    int addWaiting(WaitingDto waitingDto);
-    int modifyWaiting(WaitingDto waitingDto);
-    int deleteWaiting(int id);
+    List<WaitingDto> getWaitingList(); // 가게에 현재 등록된 모든 웨이팅 리스트
+    WaitingDto getFastWaiting(int id); // 등록한 유저보다 빠른 대기순서를 가진 유저의 팀 목록 보여줄 용도
+    int addWaiting(WaitingDto waitingDto); // 웨이팅 등록 시 대기열에 추가 및 대기 번호 생성
+    int modifyWaiting(WaitingDto waitingDto); // 웨이팅 등록 후 유저가 인원 수정
+    int deleteWaiting(int id); // 웨이팅 등록 후 유저가 취소
 
 }
