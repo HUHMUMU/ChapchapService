f660be0c34fa6666fe547df575f1ed6609a927f6
HUHMUMU <hurjun07@naver.com>
2023-04-28 16:09:38 +0900

    ajax 주소데이터 구현중

--- src/main/resources/static/public/js/StoreList.js
+++ src/main/resources/static/public/js/StoreList.js
@@ -1,10 +1,32 @@
 console.log("포함됨");
 
+let geocoder = new kakao.maps.services.Geocoder();
+let callback = function(result, status) {
+    if (status === kakao.maps.services.Status.OK) {
+        console.log(result);
+    }
+};
+
+function loadStoreList(parmas){
+    let url='/store/ajaxList.do';
+    if(parmas)url+="?"+parmas;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let stores = data.list;
+            for (let i=0; i<stores.length; i++){
+                console.log(stores[i].address);
+                geocoder.addressSearch(stores[i].address, callback);
+            }
+        });
+}
+
 let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 let options = { //지도를 생성할 때 필요한 기본 옵션
     center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
     level: 3 //지도의 레벨(확대, 축소 정도)
 };
+
 let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
 let selectedOrder = "";
@@ -25,7 +47,8 @@ function setOrder(order, selectedDirect="ASC") {
         let searchParams = new URLSearchParams(window.location.search);
         searchParams.set('order', selectedOrder);
         searchParams.set('direct', selectedDirect);
-        location.href = '?' + searchParams.toString();
+        loadStoreList(searchParams.toString());
+        //location.href = '?' + searchParams.toString();
     }
 }
 
@@ -33,7 +56,9 @@ function setMenuType(menuType, menuTypeText) {
     let searchParams = new URLSearchParams(window.location.search);
     searchParams.set('menuType', menuType);
     searchParams.set('menuName', menuTypeText);
-    location.href = '?' + searchParams.toString();
+    loadStoreList(searchParams.toString());
+
+   // location.href = '?' + searchParams.toString();
 }
 
 function setPriceRange(priceRangeValue, priceRangeText) {
