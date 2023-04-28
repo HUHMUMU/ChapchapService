539e1ffc8d2a890abc9f07bbf47dc1f2b1126b5b
KangSJ <tlssua6985@gmail.com>
2023-04-28 09:27:31 +0900

    추천가게 리스트삭제 시 새로고침 구현

--- src/main/java/com/acorn/chapspring/controller/RecommendController.java
+++ src/main/java/com/acorn/chapspring/controller/RecommendController.java
@@ -4,18 +4,21 @@ import com.acorn.chapspring.dto.RecommendStoreDto;
 import com.acorn.chapspring.dto.UserDto;
 import com.acorn.chapspring.service.RecommendService;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
 
 
 @AllArgsConstructor
 @RequestMapping("/recommend")
 @Controller
+@Log4j2
 public class RecommendController {
     private RecommendService recommendService;
 
@@ -30,6 +33,35 @@ public class RecommendController {
             model.addAttribute("recommend",recommend);
 
 
-            return "/user/detail/recommendList";
+            return "/user/detail/recommend/list";
+    }
+
+    @Data
+    class HandlerDto{
+        private int add;
+        private int remove;
+    }
+    @PostMapping("/handler.do")
+    public @ResponseBody HandlerDto addHandler(
+            @ModelAttribute RecommendStoreDto recommendstore,
+            @SessionAttribute UserDto loginUser
+        )throws IOException {
+        HandlerDto handler=new HandlerDto();
+        int add=recommendService.add(recommendstore);
+        handler.setAdd(add);
+        return handler;
+    }
+
+    @DeleteMapping("/handler.do")
+    public @ResponseBody HandlerDto removeHandler(
+            RecommendStoreDto recommendstore,
+            @SessionAttribute UserDto loginUser
+    ){
+        HandlerDto handler=new HandlerDto();
+        int remove=recommendService.delete(recommendstore);
+        handler.setRemove(remove);
+        return handler;
     }
 }
+
+
--- src/main/resources/static/public/js/Recommend.js
+++ src/main/resources/static/public/js/Recommend.js
@@ -1,11 +1,72 @@
-// const recommend=document.getElementById("recommendList");
+const recommend=document.getElementById("Recommend");
+
+async function removeRecommend(userId,storeNum){
+    let c=confirm("삭제하시겠습니까?");
+    let url="/recommend/handler.do";
+    const data=new FormData();
+    data.append("userId",userId);
+    data.append("storeNum",storeNum);
+    if(c){
+        const resp=await fetch(url,{method:"DELETE",body:data});
+        if(resp.status===200){
+            const json=await resp.json();
+            if(json.remove>0){
+                alert("삭제 성공");
+                loadRecommends(userId);
+            }else{
+                alert("삭제 실패(이미 삭제됨)");
+            }
+        }else{
+            alert("삭제 실패 status :"+resp.status);
+        }
+    }
+}
+
+async function addRecommend(userId,storeNum){
+    let url="/recommend/handler.do";
+    const data=new FormData();
+    data.append("userId",userId);
+    data.append("storeNum",storeNum);
+    const resp=await fetch(url,{method:"POST",body:data});
+    if(resp.status===200){
+        const json=await resp.json();
+        if(json.add>0){
+            alert("추가 성공!");
+        }else{
+            alert("추가 실패!");
+        }
+    }else{
+        alert("추가 실패 status : "+resp.status);
+    }
+}
+
+async function loadRecommends(userId){
+    let url=`/recommend/${userId}/list.do`;
+    const resp=await fetch(url);
+    if(resp.status===200){
+        alert("재로드 성공!");
+        let text=await resp.text();
+        // console.log(text);
+        recommend.innerHTML=text;
+    }
+}
+
+// async function toggleRecomend(userId,storeNum,btn){
+//     let active=(btn.classList.contains("active"));
+//     if(active){
 //
-// async function removeRecommend(userId){
+//     }
+// }
+
+// async function removeToggleAndRecommend(userId,storeNum){
 //     let c=confirm("삭제하시겠습니까?");
 //     let url="/recommend/handler.do";
+//     const data=new FormData();
+//     data.append("userId",userId);
+//     data.append("storeNum",storeNum);
 //     if(c){
-//         const resp=await fetch(url,{method:"DELETE"});
-//         if(resp===200){
+//         const resp=await fetch(url,{method:"DELETE",body:data});
+//         if(resp.status===200){
 //             const json=await resp.json();
 //             if(json.remove>0){
 //                 alert("삭제 성공");
@@ -16,32 +77,5 @@
 //         }else{
 //             alert("삭제 실패 status :"+resp.status);
 //         }
-//     }
-// }
-//
-// async function addRecommend(userId){
-//     let url="/recommend/handler.do";
-//     const resp=await fetch(url,{method:"POST"});
-//     if(resp.status===200){
-//         const json=await resp.json();
-//         if(json.add>0){
-//             alert("추가 성공!");
-//         }else{
-//             alert("추가 실패!");
-//         }
-//     }else{
-//         alert("추가 실패 status : "+resp.status);
-//     }
-// }
-//
-//
-//
-// async function loadRecommends(userId){
-//     let url=`/recommend/${userId}/list.do`;
-//     const resp=await fetch(url);
-//     if(resp.status===200){
-//         alert("재로드 성공!");
-//         let text=await resp.text();
-//         recommend.innerHTML=text;
 //     }
 // }
