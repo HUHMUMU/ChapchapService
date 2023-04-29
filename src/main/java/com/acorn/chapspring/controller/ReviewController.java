200e29d85f53079cc18b0351659cd79d1ecfd796
huiseon12 <huiseon12@naver.com>
2023-04-30 02:37:00 +0900

    이미지 등록 시도중

--- src/main/java/com/acorn/chapspring/controller/ReviewController.java
+++ src/main/java/com/acorn/chapspring/controller/ReviewController.java
@@ -1,12 +1,10 @@
 package com.acorn.chapspring.controller;
 
-import com.acorn.chapspring.dto.ChapstorysDto;
 import com.acorn.chapspring.dto.ReviewsDto;
 import com.acorn.chapspring.dto.StoresDto;
 import com.acorn.chapspring.dto.UserDto;
 import com.acorn.chapspring.service.ReviewService;
 import com.acorn.chapspring.service.StoreService;
-import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -15,10 +13,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-@AllArgsConstructor
+//@AllArgsConstructor
 @Controller //< @Component 요청과 응답을 처리 가능
 @RequestMapping("/review")
 @Log4j2 //log 필드로 로그남길 수 있다.(파일로 저장 가능[유지기간,성질])
@@ -26,10 +25,10 @@ public class ReviewController {
     private ReviewService reviewService;
     private StoreService storeService;
 
-//    @Value("${img.upload.path}")
-//    private String uploadPath; //등록
-//    @Value("${static.path}")
-//    private String staticPath; //삭제
+    @Value("${static.path}")
+    private String staticPath;
+
+    public ReviewController(ReviewService reviewService) {this.reviewService=reviewService;}
 
     @GetMapping("/{storeNum}/register.do")
     public String registerForm(Model model,
@@ -46,17 +45,16 @@ public class ReviewController {
         String redirectPath="redirect:/review/"+reviews.getStoreNum()+"/register.do";
         int register = 0;
         if(!loginUser.getUserId().equals(reviews.getUserId())) return redirectPath;
-//        if(!img.isEmpty()){
-//            String[] cotentTypes=img.getContentType().split("/");
-//            if(cotentTypes[0].equals("image")){
-//                String fileName=System.currentTimeMillis()+"_"+(int)(Math.random()*10000)+"."+cotentTypes[1];
-//                Path path= Paths.get(uploadPath+"/review/"+fileName);
-//                img.transferTo(path);
-//                //fetch 에서 resp.status 200 일때만 처리하기 때문에 그냥 오류가 발생하면 500
-//                reviews.setImg("/public/img/review/"+fileName);
-//            }
-//        }
-        //안녕
+        if(!img.isEmpty()){
+            String[] cotentTypes=img.getContentType().split("/");
+            if(cotentTypes[0].equals("image")){
+                String fileName=System.currentTimeMillis()+"_"+(int)(Math.random()*10000)+"."+cotentTypes[1];
+                Path path= Paths.get(staticPath+"/public/img/review/"+fileName);
+                img.transferTo(path);
+                //fetch 에서 resp.status 200 일때만 처리하기 때문에 그냥 오류가 발생하면 500
+                reviews.setImg("/public/img/review/"+fileName);
+            }
+        }
         try{
             register=reviewService.register(reviews);
         }catch (Exception e){
