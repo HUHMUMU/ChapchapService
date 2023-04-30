bca3b42aa7d406efc3a91bdd7d6d8e8b0295b1dd
jaeyeonjjang <wodus0936@gmail.com>
2023-04-30 19:28:40 +0900

    좋아요 왜 말썽 ㅠㅜ

--- src/main/resources/static/public/js/ChapstoryLike.js
+++ src/main/resources/static/public/js/ChapstoryLike.js
@@ -1,20 +1,47 @@
+// async function chapLikeHandler(cNum){
+//     const likeCont=document.getElementById("likeCont"+cNum);
+//     console.log(likeCont);
+//     let url=`/chapstory/like/${cNum}/handler.do`
+//     // let url2=`/chapstory/like/${cNum}/read.do`;
+//     const resp=await fetch(url);
+//     if(resp.status===200){
+//         const json=await resp.json();
+//         if(!json.handler){
+//             alert("성공")
+//         }else{
+//             alert(json.status+" "+json.handlerType+" 실패")
+//         }
+//     }else if(resp.status===400){
+//         alert("로그인 하셔야 이용 가능한 서비스입니다. (잘못된 요청)")
+//     }else{
+//         alert("실패 status:"+resp.status)
+//     }
+// }
+
 async function chapLikeHandler(cNum){
     const likeCont=document.getElementById("likeCont"+cNum);
     console.log(likeCont);
+    // console.log(this);
     let url=`/chapstory/like/${cNum}/handler.do`
-    let url2=`/chapstory/like/${cNum}/read.do`;
     const resp=await fetch(url);
     if(resp.status===200){
         const json=await resp.json();
-        if(!json.handler){
-            alert("성공")
+        if (json.handler>0){
+            let html=await readLike(cNum);
+            if(html){
+                likeCont.innerHTML=html;
+                alert(json.status+" "+json.handlerType+" 성공");
+            }else{
+                alert(json.status+" "+json.handlerType+" 성공(불러오기 실패 새로고침)");
+            }
         }else{
-            alert(json.status+" "+json.handlerType+" 실패")
+            alert(json.status+" "+json.handlerType+" 실패");
         }
     }else if(resp.status===400){
-        alert("로그인 하셔야 이용 가능한 서비스입니다. (잘못된 요청)")
-    }else{
-        alert("실패 status:"+resp.status)
+        alert("로그인 하셔야 이용 가능한 서비스 입니다.(잘못된 요청)")
+    }
+    else {
+        alert("실패 status:" + resp.status);
     }
 }
 
